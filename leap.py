e97ea28557dd5e93b7f6a932dd304d9a5c04cd01
PNandhakumar <34595815+PNandhakumar@users.noreply.github.com>
2017-12-16 15:09:55 +0530

    Add files via upload

---
+++ leap.py
@@ -0,0 +1,9 @@
+num=(float( input("ënter the number")))
+
+if(num%4==0):
+	
+	print("leap")
+
+else:
+	
+	print("not leap")
---
+++ vowel.py
@@ -0,0 +1,9 @@
+ch = raw_input("Enter any character: ")
+
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+	
+	print("is an alphabet.\n")
+
+else :
+	
+	print ("is not an alphabet.\n")
